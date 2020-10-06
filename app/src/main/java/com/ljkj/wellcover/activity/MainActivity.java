c1919c1b2dac64acc92fda1a501722eace7d2c9d
方志亮 <>
2020-10-06 10:49:37 +0800

    修改定位的位置

--- app/src/main/java/com/ljkj/wellcover/activity/MainActivity.java
+++ app/src/main/java/com/ljkj/wellcover/activity/MainActivity.java
@@ -1,31 +1,12 @@
 package com.ljkj.wellcover.activity;
 
-import android.Manifest;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.provider.Settings;
-import android.util.Log;
 import android.view.KeyEvent;
 
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import com.amap.api.location.AMapLocation;
-import com.amap.api.location.AMapLocationClient;
-import com.amap.api.location.AMapLocationClientOption;
-import com.amap.api.location.AMapLocationListener;
 import com.ljkj.wellcover.R;
 import com.ljkj.wellcover.fragment.HomeFragment;
 import com.ljkj.wellcover.fragment.MessageFragment;
 import com.ljkj.wellcover.fragment.MyFragment;
-import com.ljkj.wellcover.utils.Utils;
 import com.ljkj.wellcover.view.BottomBar;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 
@@ -35,34 +16,6 @@ public class MainActivity extends BaseActivity {
     BottomBar mBottomBar;
 
     private long touchTime = 0;
-
-    /**
-     * 需要进行检测的权限数组
-     */
-    protected String[] needPermissions = {
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.READ_PHONE_STATE
-    };
-
-
-    private static final int PERMISSON_REQUESTCODE = 0;
-
-    /**
-     * 判断是否需要检测，防止不停的弹框
-     */
-    private boolean isNeedCheck = true;
-
-
-    //声明AMapLocationClient类对象
-    public AMapLocationClient mLocationClient = null;
-    //声明定位回调监听器
-    //public AMapLocationListener mLocationListener = new AMapLocationListener();
-    // public AMapLocationListener mLocationListener;
-    //声明AMapLocationClientOption对象
-    public AMapLocationClientOption mLocationOption = new AMapLocationClientOption();
 
     @Override
     protected int getContentViewLayoutID() {
@@ -85,7 +38,6 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initViews() {
-
         mBottomBar.setContainer(R.id.fl_container)
                 .setTitleSize(13)
                 .setTitleBeforeAndAfterColor(getResources().getColor(R.color._363636),
@@ -104,219 +56,5 @@ public class MainActivity extends BaseActivity {
                         R.mipmap.ic_launcher)
                 .setFirstChecked(0)
                 .build();
-
-        initLocation();
-        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
-
-        startLocation();
-    }
-
-    private void startLocation() {
-        //根据控件的选择，重新设置定位参数
-        //resetOption();
-        // 设置定位参数
-        mLocationClient.setLocationOption(mLocationOption);
-        // 启动定位
-        mLocationClient.startLocation();
-    }
-
-    private void initLocation() {
-        //初始化client
-        mLocationClient = new AMapLocationClient(this.getApplicationContext());
-        //设置定位参数
-        mLocationClient.setLocationOption(getDefaultOption());
-        // 设置定位监听
-        mLocationClient.setLocationListener(mLocationListener);
-    }
-
-    /**
-     * 停止定位
-     *
-     * @author
-     * @since 2.8.0
-     */
-    private void stopLocation() {
-        // 停止定位
-        mLocationClient.stopLocation();
-    }
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        destroyLocation();
-    }
-
-    private void destroyLocation() {
-        if (null != mLocationClient) {
-            mLocationClient.onDestroy();
-            mLocationClient = null;
-            mLocationOption = null;
-
-
-        }
-    }
-
-    /**
-     * 默认的定位参数
-     *
-     * @author
-     * @since 2.8.0
-     */
-    private AMapLocationClientOption getDefaultOption() {
-        AMapLocationClientOption mOption = new AMapLocationClientOption();
-        mOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);//可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
-        mOption.setGpsFirst(false);//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
-        //mOption.setHttpTimeOut(30000);//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
-        //   mOption.setInterval(2000);//可选，设置定位间隔。默认为2秒
-        mOption.setNeedAddress(true);//可选，设置是否返回逆地理地址信息。默认是true
-        //   mOption.setOnceLocation(false);//可选，设置是否单次定位。默认是false
-        mLocationOption.setOnceLocation(true);  //获取一次定位结果：
-        mOption.setOnceLocationLatest(false);//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
-        AMapLocationClientOption.setLocationProtocol(AMapLocationClientOption.AMapLocationProtocol.HTTP);//可选， 设置网络请求的协议。可选HTTP或者HTTPS。默认为HTTP
-        mOption.setSensorEnable(false);//可选，设置是否使用传感器。默认是false
-        mOption.setWifiScan(true); //可选，设置是否开启wifi扫描。默认为true，如果设置为false会同时停止主动刷新，停止以后完全依赖于系统刷新，定位位置可能存在误差
-        mOption.setLocationCacheEnable(true); //可选，设置是否使用缓存定位，默认为true
-        return mOption;
-    }
-
-    /**
-     * 定位监听
-     */
-
-    AMapLocationListener mLocationListener = new AMapLocationListener() {
-        @Override
-        public void onLocationChanged(AMapLocation aMapLocation) {
-            if (null != aMapLocation) {
-                //解析定位结果
-                String result = Utils.getLocationStr(aMapLocation);
-                Log.e("定位", "aMapLocation is null" + result);
-            } else {
-                Log.e("定位失败", "aMapLocation is null");
-            }
-        }
-    };
-
-
-    //----------以下动态获取权限---------
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (isNeedCheck) {
-            checkPermissions(needPermissions);
-        }
-    }
-
-
-    /**
-     * 检查权限
-     *
-     * @param
-     * @since 2.5.0
-     */
-    private void checkPermissions(String... permissions) {
-        //获取权限列表
-        List<String> needRequestPermissonList = findDeniedPermissions(permissions);
-        if (null != needRequestPermissonList
-                && needRequestPermissonList.size() > 0) {
-            //list.toarray将集合转化为数组
-            ActivityCompat.requestPermissions(this,
-                    needRequestPermissonList.toArray(new String[needRequestPermissonList.size()]),
-                    PERMISSON_REQUESTCODE);
-        }
-    }
-
-
-    /**
-     * 获取权限集中需要申请权限的列表
-     *
-     * @param permissions
-     * @return
-     * @since 2.5.0
-     */
-    private List<String> findDeniedPermissions(String[] permissions) {
-        List<String> needRequestPermissonList = new ArrayList<String>();
-        //for (循环变量类型 循环变量名称 : 要被遍历的对象)
-        for (String perm : permissions) {
-            if (ContextCompat.checkSelfPermission(this,
-                    perm) != PackageManager.PERMISSION_GRANTED
-                    || ActivityCompat.shouldShowRequestPermissionRationale(
-                    this, perm)) {
-                needRequestPermissonList.add(perm);
-            }
-        }
-        return needRequestPermissonList;
-    }
-
-    /**
-     * 检测是否说有的权限都已经授权
-     *
-     * @param grantResults
-     * @return
-     * @since 2.5.0
-     */
-    private boolean verifyPermissions(int[] grantResults) {
-        for (int result : grantResults) {
-            if (result != PackageManager.PERMISSION_GRANTED) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String[] permissions, int[] paramArrayOfInt) {
-        if (requestCode == PERMISSON_REQUESTCODE) {
-            if (!verifyPermissions(paramArrayOfInt)) {      //没有授权
-                showMissingPermissionDialog();              //显示提示信息
-                isNeedCheck = false;
-            }
-        }
-    }
-
-    /**
-     * 显示提示信息
-     *
-     * @since 2.5.0
-     */
-    private void showMissingPermissionDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(R.string.notifyTitle);
-        builder.setMessage(R.string.notifyMsg);
-
-        // 拒绝, 退出应用
-        builder.setNegativeButton(R.string.cancel,
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                    }
-                });
-
-        builder.setPositiveButton(R.string.setting,
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        startAppSettings();
-                    }
-                });
-
-        builder.setCancelable(false);
-
-        builder.show();
-    }
-
-
-    /**
-     * 启动应用的设置
-     *
-     * @since 2.5.0
-     */
-    private void startAppSettings() {
-        Intent intent = new Intent(
-                Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        intent.setData(Uri.parse("package:" + getPackageName()));
-        startActivity(intent);
     }
 }
