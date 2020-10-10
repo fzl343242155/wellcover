2bf0e46270ae3e0ae5d96af0fc5b06f7b45d8c95
方志亮 <>
2020-10-10 17:58:33 +0800

    修改加载样式

--- app/src/main/java/com/ljkj/wellcover/WellCoverApplication.java
+++ app/src/main/java/com/ljkj/wellcover/WellCoverApplication.java
@@ -2,11 +2,16 @@ package com.ljkj.wellcover;
 
 import android.app.Application;
 
+import androidx.multidex.MultiDex;
+
 import com.lzy.okgo.OkGo;
 import com.lzy.okgo.cache.CacheEntity;
 import com.lzy.okgo.cache.CacheMode;
 import com.lzy.okgo.interceptor.HttpLoggingInterceptor;
 import com.lzy.okgo.model.HttpHeaders;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.footer.ClassicsFooter;
+import com.scwang.smartrefresh.layout.header.ClassicsHeader;
 
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
@@ -25,9 +30,40 @@ public class WellCoverApplication extends Application {
 
     private static WellCoverApplication mInstance = null;
 
+    //全局一次性设置默认属性和默认Header
+    static {//使用static代码段可以防止内存泄漏
+        //设置全局默认配置（优先级最低，会被其他设置覆盖）
+        SmartRefreshLayout.setDefaultRefreshInitializer((context, layout) -> {
+            //开始设置全局的基本参数（可以被下面的DefaultRefreshHeaderCreator覆盖）
+            layout.setHeaderHeight(60);//Header标准高度（显示下拉高度>=标准高度 触发刷新）
+            layout.setFooterHeight(60);//Footer标准高度（显示上拉高度>=标准高度 触发加载）
+            layout.setEnableOverScrollDrag(true);
+            layout.setHeaderMaxDragRate(2);//最大显示下拉高度/Header标准高度
+            layout.setFooterMaxDragRate(2);//最大显示下拉高度/Footer标准高度
+            layout.setEnableScrollContentWhenLoaded(true);//是否在加载完成时滚动列表显示新的内容
+            layout.setEnableFooterFollowWhenNoMoreData(true);
+            layout.setEnableFooterFollowWhenLoadFinished(true);
+            layout.setEnableNestedScroll(false);
+            layout.setEnablePureScrollMode(false);
+            layout.setDisableContentWhenRefresh(true);//是否在刷新的时候禁止列表的操作
+            layout.setDisableContentWhenLoading(true);//是否在加载的时候禁止列表的操作
+        });
+        //全局设置默认的 Header
+        SmartRefreshLayout.setDefaultRefreshHeaderCreator((context, layout) -> {
+            //开始设置全局的基本参数（这里设置的属性只跟下面的MaterialHeader绑定，其他Header不会生效，能覆盖DefaultRefreshInitializer的属性和Xml设置的属性）
+            layout.setEnableHeaderTranslationContent(true);
+            return new ClassicsHeader(context).setAccentColor(context.getResources().getColor(R.color.black));
+        });
+        SmartRefreshLayout.setDefaultRefreshFooterCreator((context, layout) -> {
+            layout.setEnableFooterTranslationContent(true);
+            return new ClassicsFooter(context).setAccentColor(context.getResources().getColor(R.color._98A3AF));
+        });
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
+        MultiDex.install(this);
         initAutoSize();
         initOkGo(this);
     }
