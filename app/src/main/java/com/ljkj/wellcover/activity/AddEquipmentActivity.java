150855b826643461636f844bc9262f78c3b7880a
方志亮 <>
2020-10-12 16:13:45 +0800

    提交数据

--- app/src/main/java/com/ljkj/wellcover/activity/AddEquipmentActivity.java
+++ app/src/main/java/com/ljkj/wellcover/activity/AddEquipmentActivity.java
@@ -283,18 +283,18 @@ public class AddEquipmentActivity extends BaseActivity implements GeoFenceListen
         mAMap.setLocationSource(this);// 设置定位监听
         mAMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
         // 自定义系统定位蓝点
-        MyLocationStyle myLocationStyle = new MyLocationStyle();
-        // 自定义定位蓝点图标
-        myLocationStyle.myLocationIcon(
-                BitmapDescriptorFactory.fromResource(R.mipmap.location));
-        // 自定义精度范围的圆形边框颜色
-        myLocationStyle.strokeColor(Color.argb(0, 0, 0, 0));
-        // 自定义精度范围的圆形边框宽度
-        myLocationStyle.strokeWidth(0);
-        // 设置圆形的填充颜色
-        myLocationStyle.radiusFillColor(Color.argb(0, 0, 0, 0));
-        // 将自定义的 myLocationStyle 对象添加到地图上
-        mAMap.setMyLocationStyle(myLocationStyle);
+//        MyLocationStyle myLocationStyle = new MyLocationStyle();
+//        // 自定义定位蓝点图标
+//        myLocationStyle.myLocationIcon(
+//                BitmapDescriptorFactory.fromResource(R.mipmap.location));
+//        // 自定义精度范围的圆形边框颜色
+//        myLocationStyle.strokeColor(Color.argb(0, 0, 0, 0));
+//        // 自定义精度范围的圆形边框宽度
+//        myLocationStyle.strokeWidth(0);
+//        // 设置圆形的填充颜色
+//        myLocationStyle.radiusFillColor(Color.argb(0, 0, 0, 0));
+//        // 将自定义的 myLocationStyle 对象添加到地图上
+//        mAMap.setMyLocationStyle(myLocationStyle);
         mAMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
         // 设置定位的类型为定位模式 ，可以由定位、跟随或地图根据面向方向旋转几种
         mAMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
